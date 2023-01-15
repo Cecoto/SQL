86231014b6ed780d78e9ec3b8464e59ccfc69795
Tsvetomir <cecohristov97@gmail.com>
2023-01-16 00:02:52 +0200

    query exercise

---
+++ Many-to-many relation.sql
@@ -0,0 +1,21 @@
+CREATE TABLE [Students](
+	StudentID INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL
+)
+CREATE TABLE [Exams](
+	ExamID INT PRIMARY KEY IDENTITY(101,1) NOT NULL,
+	[Name] NVARCHAR(70) NOT NULL
+)
+CREATE TABLE [StudentsExams](
+	StudentID INT,
+	ExamID INT,
+	CONSTRAINT PK_StudentsExams
+	PRIMARY KEY (StudentID,ExamID),
+	CONSTRAINT FK_StudentsExams_Students
+	FOREIGN KEY(StudentID) 
+	REFERENCES[Students]([StudentID]),
+	CONSTRAINT FK_StudentsExams_Exams
+	FOREIGN KEY(ExamID)
+	REFERENCES [Exams]([ExamID])
+	
+)
