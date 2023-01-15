84aa9f78ca6b7c7b836beadcaadabeea153805ec
Tsvetomir <cecohristov97@gmail.com>
2023-01-16 00:02:39 +0200

    query exercise

---
+++ One-to-many relation .sql
@@ -0,0 +1,10 @@
+CREATE TABLE Manufacturers(
+	[ManufacturerID] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] NVARCHAR(30) NOT NULL,
+	[EstablishedOn] DATE NOT NULL,
+)
+CREATE TABLE Models(
+	[ModelID] INT PRIMARY KEY IDENTITY(101,1) NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
+	[ManufacturerID] INT FOREIGN KEY REFERENCES [Manufacturers]([ManufacturerID]) NOT NULL
+)
