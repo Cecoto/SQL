11df3f822b30223b55158092fffd0ec838b4efb7
Cecoto <cvetomir97@abv.bg>
2023-02-15 23:49:43 +0200

    Exam preparation exercise

---
+++ ExamPrep - 13 Feb/TASK04 - DELETE.sql
@@ -0,0 +1,10 @@
+
+DELETE RepositoriesContributors
+WHERE RepositoryId = 
+(SELECT Id FROM Repositories
+WHERE [Name] = 'Softuni-Teamwork')
+
+DELETE Issues
+WHERE RepositoryId = 
+(SELECT Id FROM Repositories
+WHERE [Name] = 'Softuni-Teamwork')
