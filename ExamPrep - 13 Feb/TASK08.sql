eecebf0d482bc62b4da778d376248ec7bfac69e1
Cecoto <cvetomir97@abv.bg>
2023-02-16 23:48:17 +0200

    Exam exercise

---
+++ ExamPrep - 13 Feb/TASK08.sql
@@ -0,0 +1,8 @@
+SELECT
+fp.Id, 
+fp.[Name],
+CONCAT(fp.Size,'KB') AS Size
+FROM Files AS fch
+FULL OUTER JOIN Files AS fp ON fch.ParentId = fp.Id
+WHERE fch.Id IS NULL
+ORDER BY fp.Id,fp.[Name],fp.Size DESC
