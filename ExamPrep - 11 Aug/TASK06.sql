35aa3c6982431c9cd70a0880de5ab8eb099f368e
Cecoto <cvetomir97@abv.bg>
2023-02-22 23:27:27 +0200

    query exercise

---
+++ ExamPrep - 11 Aug/TASK06.sql
@@ -0,0 +1,11 @@
+SELECT
+f.ProductId,
+f.Rate,
+f.[Description],
+c.Id AS  CustomerId,
+c.Age,
+c.Gender
+FROM Feedbacks AS f
+JOIN Customers AS c ON f.CustomerId = c.Id
+WHERE f.Rate <5.0
+ORDER BY f.ProductId DESC,f.Rate ASC
