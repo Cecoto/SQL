37a7a5fcbb0e8c9ec2003f2428620162f3e14e3c
Cecoto <cvetomir97@abv.bg>
2023-02-22 23:27:57 +0200

    query exercise

---
+++ ExamPrep - 11 Aug/TASK08.sql
@@ -0,0 +1,9 @@
+SELECT 
+c.FirstName,
+c.Age,
+c.PhoneNumber
+FROM Customers AS c 
+JOIN Countries AS co ON c.CountryId = co.Id
+WHERE c.Age >=21 AND c.FirstName LIKE '%an%'
+OR c.PhoneNumber LIKE '%38' AND co.[Name] != 'Greece'
+ORDER BY c.FirstName,c.Age DESC
