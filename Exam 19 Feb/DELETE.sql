620481a1a2c9b16710cd4095714bd2e989943dd6
Cecoto <cvetomir97@abv.bg>
2023-03-01 23:47:33 +0200

    sql exercise

---
+++ Exam 19 Feb/DELETE.sql
@@ -0,0 +1,33 @@
+DELETE CreatorsBoardgames
+WHERE BoardgameId IN(
+SELECT Id
+FROM Boardgames
+WHERE PublisherId IN(
+SELECT Id
+FROM Publishers
+WHERE AddressId IN(
+SELECT Id
+FROM Addresses 
+WHERE Town LIKE 'L%')))
+
+DELETE Boardgames
+WHERE PublisherId IN(
+SELECT Id
+FROM Publishers
+WHERE AddressId IN(
+SELECT Id
+FROM Addresses 
+WHERE Town LIKE 'L%')
+)
+
+
+DELETE Publishers
+WHERE AddressId IN(
+SELECT Id
+FROM Addresses 
+WHERE Town LIKE 'L%')
+
+
+
+DELETE Addresses
+WHERE Town LIKE 'L%'
